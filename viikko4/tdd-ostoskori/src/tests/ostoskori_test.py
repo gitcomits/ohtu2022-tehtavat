188cab4d8cf9ed7ca104778c61d1d11676fd200e
Jukka <jukka.mustonen@helsinki.fi>
2022-02-09 19:15:03 +0200

    kahden tuotteen lisaaminen palauttaa kaksi ostos oliota

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -62,5 +62,13 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 1)
         
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_ostosolio(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        piima = Tuote("piima", 3)
+        self.kori.lisaa_tuote(piima)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 2)
+        # testaa että metodin palauttaman listan pituus 2
 
         
