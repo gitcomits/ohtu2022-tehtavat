a4364d633b54509b49968cfb7a257cccf173045e
Jukka <jukka.mustonen@helsinki.fi>
2022-02-15 19:11:06 +0200

    class Or

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, Or
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
@@ -13,10 +13,12 @@ def main():
 #        PlaysIn("PHI")
 #    )
 
-    matcher = And(
-        HasFewerThan(1, "goals"),
-        PlaysIn("NYR")
+    matcher = Or(
+        HasAtLeast(30, "goals"),
+        HasAtLeast(50, "assists")
     )
+
+
 
 
 
